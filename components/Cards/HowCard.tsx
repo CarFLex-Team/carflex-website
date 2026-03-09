f76ad17ae20fa9eaa3fe00976d822add0ce5c154
Mazen <Mazenessam2011@outlook.com>
2026-03-09 17:50:03 +0200

    make components in about and edit mail

---
+++ components/Cards/HowCard.tsx
@@ -0,0 +1,27 @@
+export default function HowCard({
+  title,
+  description,
+  imgLink,
+  imgAlt,
+  isReversed = false,
+}: {
+  title: string;
+  description: string;
+  imgLink: string;
+  imgAlt: string;
+  isReversed?: boolean;
+}) {
+  return (
+    <div className={`flex flex-col md:flex-row  w-full items-center justify-center gap-15 my-10 ${isReversed ? 'md:flex-row-reverse' : ''}`}>
+      <div className="md:w-1/2 w-full">
+        <p className="text-gray-700 text-2xl font-bold  ">{title}</p>
+        <p className="text-gray-700 text-md  ">{description}</p>
+      </div>
+      <img
+        src={imgLink}
+        alt={imgAlt}
+        className="max-w-2/3 md:max-w-1/3 h-auto rounded-lg shadow-md"
+      />
+    </div>
+  );
+}
