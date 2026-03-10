41a8b9f4217a76346e9078d26ddc5b7ff5e39a1a
Mazen Essam <mazenessam2011@outlook.com>
2026-03-10 23:04:31 +0200

    add video and change text in main with motion

--- components/Sections/Main.tsx
+++ components/Sections/Main.tsx
@@ -1,23 +1,43 @@
+"use client";
 import { Star } from "lucide-react";
 import SellCarContainer from "./SellCarContainer";
 import GoogleIcon from "../Icons/googleIcon";
+import { motion } from "framer-motion";
+import { Anton } from "next/font/google";
+const anton = Anton({ subsets: ["latin"], weight: "400" });
 
 export default function Main() {
   return (
     <>
-      <div className="relative flex items-center justify-center h-[70vh] p-4 bg-secondary-800">
-        <div
-          className="absolute inset-0 bg-cover bg-center opacity-20 "
-          style={{ backgroundImage: `url('imgs/home.png')` }}
+      <div className="relative flex items-center justify-center h-[80vh] p-4 bg-secondary-800">
+        <video
+          className="absolute inset-0 w-full h-full object-cover object-top opacity-30"
+          src="/video/car_video.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
         />
         <div className="relative z-10 flex flex-col w-full gap-10">
-          <div className=" flex flex-col gap-4 ml-2 md:ml-10">
-            <h1 className="text-3xl md:text-5xl  text-white ">
-              Easiest Way To Sell a Car
-            </h1>
-            <h3 className="text-base md:text-2xl  text-white z-10">
-              Sell your car directly to Carflex Plus.
-            </h3>
+          <div
+            className={`flex flex-col gap-4 ml-2 md:ml-10 ${anton.className}`}
+          >
+            <motion.h1
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.6 }}
+              className={`text-7xl  text-white `}
+            >
+              SELL YOUR CAR
+            </motion.h1>
+            <motion.h1
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: 0.2 }}
+              className="text-7xl  text-white "
+            >
+              WITH ONLY <span className="text-secondary-400">A CLICK</span>
+            </motion.h1>
             <a
               className="flex items-center gap-1"
               href="https://maps.app.goo.gl/YFt5zTJs8dJy7o2i9"
@@ -31,14 +51,13 @@ export default function Main() {
                 <Star fill="orange" stroke="none" size={20} />
                 <Star fill="orange" stroke="none" size={20} />
               </div>
-              <p className="text-white text-lg font-semibold text-center">
-                4.9
-              </p>
+              <p className="text-white text-lg  text-center">4.9</p>
             </a>
           </div>
         </div>
       </div>
-      <div className="relative -mt-13 z-10 flex items-center justify-center px-3 sm:px-8 md:px-15 ">
+
+      <div className="relative -mt-25 z-10 flex items-center justify-center px-3 sm:px-8 md:px-15 ">
         <SellCarContainer />
       </div>
     </>
