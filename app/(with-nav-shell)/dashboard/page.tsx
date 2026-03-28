7bb6343ed62063b7738a4bd68174f845184fa6af
Mazen Essam <mazenessam2011@outlook.com>
2026-03-28 18:33:14 +0200

    add placeholder for dashboard

---
+++ app/(with-nav-shell)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+      <p>
+        Welcome to your dashboard! Here you can manage your account and view
+        your activity.
+      </p>
+    </div>
+  );
+}
